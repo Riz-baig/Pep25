6d36e5a2b1a9d657fd3bf4af7ec4457fce971d57
Riz-baig <baigrizmirza@gmial.com>
2025-10-07 10:29:17 +0200

    Reorganizado nuevamente

---
+++ U3/U3_programas_01/programa_02.py
@@ -0,0 +1,17 @@
+# Escribe un programa en Python que haga uso de una función llamada saludar que cumpla 
+#los siguientes requisitos:
+# • Entrada: Tiene 4 parámetros de entrada, que serán 4 cadenas de texto: nombre, 
+#   primer apellido, segundo apellido y  curso. El parámetro curso  tendrá en la 
+#   declaración de la función el valor por defecto  “2DAW”
+# • Salida: No tiene parámetros de salida.
+# • Funcionalidad: Imprime por pantalla un mensaje saludando al alumno/a que se 
+#   haya pasado como parámetro de entrada. 
+#   El programa debe invocar a la función varias veces. En algunas de ellas se tiene que usar 
+#   el paso de parámetros de  forma nominal (con clave valor).
+curso = "2DAW"
+def saludar(nombre, apellido1, apellido2, curso):
+    
+    print(nombre, "con apellidos ", apellido1, apellido2, "esta en el curso de ", curso)
+    
+
+saludar("Elia", "Yun", "Gil", curso)
